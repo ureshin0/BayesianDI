92060fb7470e89e089293f62edc59aa4b3bce0f4
ureshin0 <yamatonmail@g.ecc.u-tokyo.ac.jp>
2026-02-25 12:59:38 +0900

    Update import path for physics module

--- moments.py
+++ moments.py
@@ -12,7 +12,7 @@ import healpy as hp
 import healjax as hj
 
 # Import core physics and matrix functions
-from physics import (
+from src.physics import (
     angular_distance, 
     get_full_design_matrix, 
     get_exposure_matrix
